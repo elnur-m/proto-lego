3a1273bd5147b5968ca4aa4fd3308cc8c33dd852
Elnur Mammadov <eln.mamedov@gmail.com>
2023-06-11 16:28:14 +0400

    Replaced abstract methods with virtuals

--- build/Build.cs
+++ build/Build.cs
@@ -31,7 +31,7 @@ internal class Build : NukeBuild
     [Parameter][Secret] private readonly string NuGetApiKey;
 
     private AbsolutePath PackagesDirectory => TemporaryDirectory / "packages";
-    private const string Version = "1.0.0-alpha.11";
+    private const string Version = "1.0.0-alpha.12";
 
     private Target Clean => _ => _
         .Executes(() =>
