650ceda556b9b2dc8981a978f44ad940dc6c3513
Elnur Mamedov <eln.mamedov@outlook.com>
2023-06-12 14:50:50 +0400

    Extracted sequences into Client classes

---
+++ src/Proto.Lego/Aggregate/IAggregateClient.cs
@@ -0,0 +1,13 @@
+﻿using Google.Protobuf.WellKnownTypes;
+
+namespace Proto.Lego.Aggregate;
+
+public interface IAggregateClient<TClient> : IAggregateClient
+{
+    static abstract TClient Create(Cluster.Cluster cluster, string identity, string caller);
+}
+
+public interface IAggregateClient
+{
+    Task<Empty?> ClearAsync(CancellationToken cancellationToken);
+}
