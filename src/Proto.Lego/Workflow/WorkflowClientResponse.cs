d73285f521500eb580e2d61add1b9c387e0d6bc1
Elnur Mammadov <eln.mamedov@gmail.com>
2023-06-12 19:09:09 +0400

    Changed Workflow's response type

---
+++ src/Proto.Lego/Workflow/WorkflowClientResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Proto.Lego.Workflow;
+
+public class WorkflowClientResponse<TInput>
+{
+    public WorkflowClientResponse(TInput input, WorkflowResult result)
+    {
+        Input = input;
+        Result = result;
+    }
+
+    public TInput Input { get; set; }
+    public WorkflowResult Result { get; set; }
+}
